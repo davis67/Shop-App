beab59b2d8766c237fc4f14e88cb62f473b28126
ArindaRodrick <arinda.rodrick@gmail.com>
2023-11-06 19:12:34 +0300

    Added files

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -2,6 +2,7 @@
 
 <title>Shopping list</title>
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+<link rel="stylesheet" href="css/style.css">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
@@ -22,7 +23,7 @@
     }
 </style>
 
-<body style="font-family: Open Sans, sans-serif">
+<body class="login" style="font-family: Open Sans, sans-serif">
     <section class="px-6 py-8">
         {{ $slot }}
     </section>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,7 +2,7 @@
 
 <title>Shopping list</title>
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-<link rel="stylesheet" href="css/style.css">
+
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
@@ -23,7 +23,7 @@
     }
 </style>
 
-<body style="font-family: Open Sans, sans-serif">
+<body style="font-family: Open Sans, sans-serif ">
     <section class="px-6 py-8">
         <nav class="md:flex md:justify-between md:items-center bg-gray-300">
             <div>
